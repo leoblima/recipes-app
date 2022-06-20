fb969f50780ce5ec2933aacc28ae70669fc2ac6e
Caio Imbroisi <caio.imbroisi@gmail.com>
2022-06-20 19:12:41 -0300

    adicionado página de login e sua lógica de funcionamento

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import rockGlass from './images/rockGlass.svg';
+// import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="meals">
+      <Login />
+      {/* <div className="meals">
         <span className="logo">TRYBE</span>
         <object
           className="rocksGlass"
@@ -16,7 +18,7 @@ function App() {
         >
           Glass
         </object>
-      </div>
+      </div> */}
     </BrowserRouter>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,87 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Context } from '../context/context';
+import './Login.css';
+
+function Login() {
+  const [disabled, setDisabled] = useState(true);
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+  } = useContext(Context);
+
+  const checkPassword = (pwd) => {
+    const minChar = 6;
+    return pwd.length > minChar;
+  };
+
+  const checkEmail = (mail) => {
+    const emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailFormat.test(mail);
+  };
+
+  const history = useHistory();
+
+  const handleSubmit = () => {
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    const obj = {
+      email,
+    };
+    localStorage.setItem('user', JSON.stringify(obj));
+    history.push('/foods');
+  };
+
+  useEffect(() => {
+    const minPassword = checkPassword(password);
+    const minEmail = checkEmail(email);
+    console.log(minPassword);
+    if (minPassword && minEmail) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [email, password]);
+
+  return (
+    <div className="login-main">
+      <form className="login">
+        <h3>
+          Login
+        </h3>
+        <label htmlFor="email">
+          <input
+            name="email"
+            type="text"
+            label="E-mail"
+            data-testid="email-input"
+            onChange={ (e) => setEmail(e.target.value) }
+          />
+        </label>
+        <br />
+        <label htmlFor="password">
+          <input
+            name="password"
+            type="password"
+            label="Password"
+            onChange={ (e) => setPassword(e.target.value) }
+            data-testid="password-input"
+          />
+        </label>
+        <br />
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+          disabled={ disabled }
+          onClick={ handleSubmit }
+        >
+          Enter
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
