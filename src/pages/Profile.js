80caaa6b218b911a128ce6fbd596b1536fc6817c
Felipe Avila <f.avila@hotmail.com.br>
2022-06-20 17:21:37 -0300

    Finalizado bloco de Perfil - Falta ajustar a exibição do email vindo do localstorage

---
+++ src/pages/Profile.js
@@ -0,0 +1,64 @@
+import { React } from 'react';
+import { useHistory } from 'react-router-dom';
+// import { Context } from '../context/context';
+
+const Profile = () => {
+  // const { email } = useContext(Context);
+  const history = useHistory();
+
+  const redirectToFavorite = () => {
+    history.push('/favorite-recipes');
+  };
+
+  const redirectToDoneRecipes = () => {
+    history.push('/done-recipes');
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
+  return (
+    <div
+      className="d-flex flex-column align-items-center"
+    >
+      <p data-testid="profile-email">
+        email@mail.com
+      </p>
+      <div>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          className="btn btn-dark"
+          onClick={ redirectToDoneRecipes }
+        >
+          Done Recipes
+        </button>
+      </div>
+
+      <div>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          className="btn btn-dark"
+          onClick={ redirectToFavorite }
+        >
+          Favorite Recipes
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          className="btn btn-dark"
+          onClick={ handleLogout }
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
