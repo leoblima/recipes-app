ca8e17a08043e727011b603a8bb34611b6773091
leoblima <leoblima00@gmail.com>
2022-06-20 20:29:36 -0300

    Envia botões com a funcionalidade necessária - req 11

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,13 @@
-import { React, useContext } from 'react';
+import { React, useContext, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Context } from '../context/context';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header() {
-  const {
-    location: { pathname },
-  } = useHistory();
-
-  const getPageName = (pathname) => {
+  const history = useHistory();
+  const [search, setSearch] = useState(false);
+  const getPageName = ({ location: { pathname } }) => {
     switch (pathname) {
       case '/':
         return 'Home Page';
@@ -20,19 +18,36 @@ function Header() {
     }
   };
 
+  const goToProfilePage = (e) => {
+    e.preventDefault();
+    history.push('/profile');
+  };
+
+  const getSearchBar = (e) => {
+    e.preventDefault();
+    setSearch(!search);
+  };
+
   return (
     <div>
-      <h3 data-testsid='page-title'>{getPageName(pathname)}</h3>
+      <h3 data-testsid='page-title'>{getPageName(history)}</h3>
       <button>
         <img
           src={profileIcon}
           alt='profile-icon'
           data-testsid='profile-top-btn'
+          onClick={(e) => goToProfilePage(e)}
         />
       </button>
       <button>
-        <img src={searchIcon} alt='seacrh-icon' data-testsid='search-top-btn' />
+        <img
+          src={searchIcon}
+          alt='seacrh-icon'
+          data-testsid='search-top-btn'
+          onClick={(e) => getSearchBar(e)}
+        />
       </button>
+      {search && <h1>SearchBar</h1>}
     </div>
   );
 }
