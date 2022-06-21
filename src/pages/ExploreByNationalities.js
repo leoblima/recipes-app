8c7890e000beb64373031b6af4f366a2ef1d0fb9
leoblima <leoblima00@gmail.com>
2022-06-21 15:07:09 -0300

    Envia estrutura para criação da página e rota criada

---
+++ src/pages/ExploreByNationalities.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BottomMenu from '../components/BottomMenu';
+import Header from '../components/Header';
+
+const ExploreByNationalities = () => (
+  <div>
+    <Header />
+    <BottomMenu />
+  </div>
+
+);
+
+export default ExploreByNationalities;
