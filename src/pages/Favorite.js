b7791ba48a304df37aee168a45e29a59b1eed4e0
leoblima <leoblima00@gmail.com>
2022-06-22 20:24:07 -0300

    Envia requisitos 61, 62, 63, 65 e 66. básicamente utilizando o mesmo código do done-recipes

--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -6,30 +6,30 @@ import DoneRecipeCard from '../components/DoneRecipeCard';
 function Favorite() {
   const [category, setCategory] = useState('All');
   const [isEnable, setIsEnable] = useState(true);
-  // const doneRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
-  const doneRecipes = [{
-    id: 'id-da-receita',
-    type: 'drink',
-    nationality: 'nacionalidade-da-receita-ou-texto-vazio',
-    category: 'categoria-da-receita-ou-texto-vazio',
-    alcoholicOrNot: 'alcoholic-ou-non-alcoholic-ou-texto-vazio',
-    name: 'nome-da-receita',
-    image: 'imagem-da-receita',
-    doneDate: 'quando-a-receita-foi-concluida',
-    tags: ['array-de-tags-da-receita-ou-array-vazio', 2, 'tres', 'quatro'],
-  },
-  {
-    id: 'id-da-receita2',
-    type: 'food',
-    nationality: 'nacionalidade-da-receita-ou-texto-vazio2',
-    category: 'categoria-da-receita-ou-texto-vazio',
-    alcoholicOrNot: 'alcoholic-ou-non-alcoholic-ou-texto-vazio',
-    name: 'nome-da-receita2',
-    image: 'imagem-da-receita2',
-    doneDate: 'quando-a-receita-foi-concluida2',
-    tags: ['array-de-tags-da-receita-ou-array-vazio', 2, 'tres', 'quatro'],
-  },
-  ];
+  const doneRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  // const doneRecipes = [{
+  //   id: 'id-da-receita',
+  //   type: 'drink',
+  //   nationality: 'nacionalidade-da-receita-ou-texto-vazio',
+  //   category: 'categoria-da-receita-ou-texto-vazio',
+  //   alcoholicOrNot: 'alcoholic-ou-non-alcoholic-ou-texto-vazio',
+  //   name: 'nome-da-receita',
+  //   image: 'imagem-da-receita',
+  //   doneDate: 'quando-a-receita-foi-concluida',
+  //   tags: ['array-de-tags-da-receita-ou-array-vazio', 2, 'tres', 'quatro'],
+  // },
+  // {
+  //   id: 'id-da-receita2',
+  //   type: 'food',
+  //   nationality: 'nacionalidade-da-receita-ou-texto-vazio2',
+  //   category: 'categoria-da-receita-ou-texto-vazio',
+  //   alcoholicOrNot: 'alcoholic-ou-non-alcoholic-ou-texto-vazio',
+  //   name: 'nome-da-receita2',
+  //   image: 'imagem-da-receita2',
+  //   doneDate: 'quando-a-receita-foi-concluida2',
+  //   tags: ['array-de-tags-da-receita-ou-array-vazio', 2, 'tres', 'quatro'],
+  // },
+  // ];
   const getRecipeByType = (recipe) => (recipe.type === category);
 
   const renderRecipes = (recipe, index) => (
