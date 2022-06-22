81c5c34c88d33abd334d9d9ad2e7815a7867ed2b
leoblima <leoblima00@gmail.com>
2022-06-22 15:25:35 -0300

    Envia colocação dos botões na página de done recipes

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,9 +1,11 @@
 import { React } from 'react';
 import Header from '../components/Header';
+import ButtonsDoneRecipes from '../components/ButtonsDoneRecipes';
 
 const DoneRecipes = () => (
   <div>
     <Header />
+    <ButtonsDoneRecipes />
 
   </div>
 );
